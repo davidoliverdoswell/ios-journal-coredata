f54ee6f65687aba0ff65550c13b2023fc6e3c228
David Doswell <davidoliverdoswell@icloud.com>
2019-01-22 16:36:46 -0800

    set up entry controller

--- ios-journal-coredata/ios-journal-coredata/EntryController.swift
+++ ios-journal-coredata/ios-journal-coredata/EntryController.swift
@@ -7,3 +7,52 @@
 //
 
 import Foundation
+import CoreData
+
+class EntryController {
+    
+    var entries: [Entry] {
+        return loadFromPersistentStore()
+    }
+    
+    func create(with title: String, bodyText: String, timestamp: Date, identifier: String) {
+        
+        let entry = Entry(title: title, bodyText: bodyText, timestamp: timestamp, identifier: identifier)
+        
+        entry.title = title
+        entry.bodyText = bodyText
+        entry.timestamp = timestamp
+        entry.identifier = identifier
+        
+        saveToPersistentStore()
+    }
+    
+    func delete(entry: Entry) {
+        CoreDataStack.shared.mainContext.delete(entry)
+        saveToPersistentStore()
+    }
+    
+    func saveToPersistentStore() {
+        do {
+            try CoreDataStack.shared.mainContext.save()
+        } catch {
+            NSLog("Failed to save entry in context: \(error)")
+        }
+    }
+    
+    func loadFromPersistentStore() -> [Entry] {
+        let fetchRequest: NSFetchRequest<Entry> = Entry.fetchRequest()
+        
+        do {
+            var results = [Entry]()
+            CoreDataStack.shared.mainContext.performAndWait {
+                results = (try? fetchRequest.execute()) ?? []
+            }
+            return results
+        }
+    }
+    
+    
+    
+    
+}
